e5d09a26bd2748e1792f3a3ffb140f76ab87f46b
Raphael Okeibunor <raphaelnagato19@gmail.com>
2021-06-18 00:17:00 +0100

    Add Payment Controller and remove unneccessary usings and files

---
+++ ProcessPayment.API/Controllers/PaymentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProcessPayment.Dto;
+
+
+namespace ProcessPayment.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        [HttpPost("process-payment")]
+        public IActionResult ProcessPayment([FromBody] PaymentDetailsDto paymentDetails)
+        {
+            return Ok();
+        }
+    }
+}
