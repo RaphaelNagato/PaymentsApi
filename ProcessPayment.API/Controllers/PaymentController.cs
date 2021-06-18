580b0d23920e13bb4931160032f83205098d32a0
Raphael Okeibunor <raphaelnagato19@gmail.com>
2021-06-18 02:43:35 +0100

    Test Validation

--- ProcessPayment.API/Controllers/PaymentController.cs
+++ ProcessPayment.API/Controllers/PaymentController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProcessPayment.Commons;
 using ProcessPayment.Dto;
-
+using ProcessPayment.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace ProcessPayment.API.Controllers
 {
@@ -9,9 +12,21 @@ namespace ProcessPayment.API.Controllers
     public class PaymentController : ControllerBase
     {
         [HttpPost("process-payment")]
-        public IActionResult ProcessPayment([FromBody] PaymentDetailsDto paymentDetails)
+        public async Task<IActionResult> ProcessPayment([FromBody] PaymentDetailsDto paymentDetails)
         {
-            return Ok();
+            List<KeyValue> listOfErrors = await Helper.ValidatePaymentDetails(paymentDetails);
+            Response result = new Response();
+
+            if (listOfErrors.Count != 0)
+            {
+                result.Message = "request is invalid";
+                result.Errors = listOfErrors;
+                return BadRequest(result);
+            }
+
+            result.Message = "request is successful";
+            return Ok(result);
+
         }
     }
 }
