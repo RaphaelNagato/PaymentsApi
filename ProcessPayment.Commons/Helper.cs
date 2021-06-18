6e304b882ec8ce2882baae383fac195f5ddbd8e5
Raphael Okeibunor <raphaelnagato19@gmail.com>
2021-06-18 02:42:14 +0100

    Add Validator for incoming payment details

---
+++ ProcessPayment.Commons/Helper.cs
@@ -0,0 +1,35 @@
+﻿using ProcessPayment.Commons.Validators;
+using ProcessPayment.Dto;
+using ProcessPayment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessPayment.Commons
+{
+    public static class Helper
+    {
+        /// <summary>
+        /// Validate Incoming Payment Details using Fluent Api
+        /// </summary>
+        /// <param name="paymentDetails"></param>
+        /// <returns></returns>
+        public static async Task<List<KeyValue>> ValidatePaymentDetails(PaymentDetailsDto paymentDetails)
+        {
+            List<KeyValue> validationErrors = new List<KeyValue>();
+            var validator = new PaymentDetailsValidator();
+            var results = await validator.ValidateAsync(paymentDetails);
+            if(!results.IsValid)
+            {
+                validationErrors.AddRange(results.Errors.Select(error => new KeyValue
+                { 
+                    PropertyName = error.PropertyName,
+                    PropertyValue = error.ErrorMessage,
+                }));
+            }
+            return validationErrors;
+        }
+    }
+}
---
+++ ProcessPayment.Commons/Validators/PaymentDetailsValidator.cs
@@ -0,0 +1,39 @@
+﻿using FluentValidation;
+using ProcessPayment.Dto;
+using System;
+using System.Text.RegularExpressions;
+
+namespace ProcessPayment.Commons.Validators
+{
+    public class PaymentDetailsValidator : AbstractValidator<PaymentDetailsDto>
+    {
+        public PaymentDetailsValidator()
+        {
+            RuleFor(x => x.CreditCardNumber).Cascade(CascadeMode.Stop).NotEmpty().WithMessage("Credit card number cannot be empty").CreditCard();
+            RuleFor(x => x.CardHolder).Cascade(CascadeMode.Stop).NotEmpty().WithMessage("Card Holder cannot be empty").Length(3,25)
+                .Matches("^[A-Za-z]+[ ]?[A-Za-z]+[ ]?[A-Za-z]+$").WithMessage("Card Holder Name not in correct format.");
+            RuleFor(x => x.ExpirationDate).Cascade(CascadeMode.Stop).NotEmpty().WithMessage("Expiration Date cannot be empty")
+                .Must(IsValidDate).WithMessage("Card is expired");
+            RuleFor(x => x.Amount).Cascade(CascadeMode.Stop).NotEmpty().WithMessage("Amount cannot be empty")
+                .Must(IsValidAmount).WithMessage("Amount must be positive and greater than zero");
+            RuleFor(x => x.SecurityCode).Cascade(CascadeMode.Stop).Length(3).Must(code => int.TryParse(code, out _)).When(x => !string.IsNullOrWhiteSpace(x.SecurityCode))
+                .WithMessage("Security Code not in correct format");
+        }
+        private bool IsValidDate(DateTime date)
+        {
+            if(date <= DateTime.Now)
+            {
+                return false;
+            }
+            return true;
+        }
+        private bool IsValidAmount(decimal amount)
+        {
+            if(amount <= 0)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
