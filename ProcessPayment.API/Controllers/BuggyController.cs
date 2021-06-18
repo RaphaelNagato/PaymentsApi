623ef72e74eb0c5ca077255f06594a665e9f4942
Raphael Okeibunor <raphaelnagato19@gmail.com>
2021-06-19 00:25:16 +0100

    add controller to simulate internal server error

---
+++ ProcessPayment.API/Controllers/BuggyController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProcessPayment.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BuggyController : ControllerBase
+    {
+
+        [HttpGet("servererror")]
+        public ActionResult GetServerError()
+        {
+            string thing = null;
+            var thingToReturn = thing.ToString();
+            return Ok();
+        }
+    }
+}
