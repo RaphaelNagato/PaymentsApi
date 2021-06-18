7e6b6684b281e42a3b660e5ec5b857ecae072277
Raphael Okeibunor <raphaelnagato19@gmail.com>
2021-06-18 02:43:03 +0100

    Create Response Object and KeyValue Object

---
+++ ProcessPayment.Models/KeyValue.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProcessPayment.Models
+{
+    public class KeyValue
+    {
+        public string PropertyName { get; set; }
+        public string PropertyValue { get; set; }
+    }
+}
---
+++ ProcessPayment.Models/Response.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace ProcessPayment.Models
+{
+    public class Response
+    {
+        [DefaultValue(false)]
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public IEnumerable<KeyValue> Errors { get; set; }
+    }
+}
